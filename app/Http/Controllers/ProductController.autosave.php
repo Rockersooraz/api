9fa9bfc7034ab83156f3bb08a4cbb34c28935f67
unknown <rockersooraz80@gmail.com>
2018-08-25 17:48:02 +0545

    update part is done

--- app/Http/Controllers/ProductController.autosave.php
+++ app/Http/Controllers/ProductController.autosave.php
@@ -93,7 +93,15 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail']=$request->description;
+        unset($request['description']); 
+         $product->update($request->all());
+
+         return response([
+        'data'=>new productResource($product)
+      
+     ],Response::HTTP_CREATED);
+
     }
 
     /**
